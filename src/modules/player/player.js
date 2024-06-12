455edfb346e90a5eacb90bb401a017d433fe24be
nico-von <131730901+nico-von@users.noreply.github.com>
2024-06-12 23:28:02 +0800

    feat: Ensure players have gameboard object

--- src/modules/player/player.js
+++ src/modules/player/player.js
@@ -1,5 +1,8 @@
+import { Gameboard } from "../gameboard/gameboard";
+
 export class Player {
-    constructor(isComputer) {
+    constructor(isComputer, gameBoardSize) {
         this.isComputer = isComputer;
+        this.gameboard = new Gameboard(gameBoardSize);
     }
 }
--- src/tests/player/player.test.js
+++ src/tests/player/player.test.js
@@ -1,10 +1,15 @@
+import { Gameboard } from "../../modules/gameboard/gameboard";
 import { Player } from "../../modules/player/player";
 
 describe("Player creation", () => {
     test("create a player computer class", () => {
-        expect(new Player(true)).toHaveProperty("isComputer", true);
+        expect(new Player(true, 10)).toHaveProperty("isComputer", true);
     })
     test("create a player person class", () => {
-        expect(new Player(false)).toHaveProperty("isComputer", false);
+        expect(new Player(false, 10)).toHaveProperty("isComputer", false);
+    })
+    test("player must have a gameboard object", () => {
+        const player = new Player(false, 10);
+        expect(player.gameboard instanceof Gameboard).toBe(true);
     })
 })
